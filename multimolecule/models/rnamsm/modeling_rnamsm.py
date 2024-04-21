461ba058860911021cb51d3f4aca5cdceb966036
Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>
2024-04-21 21:23:40 +0800

    fix attention mask of RnaMsm
    Signed-off-by: Zhiyuan Chen <chenzhiyuan@pjlab.org.cn>

--- multimolecule/models/rnamsm/modeling_rnamsm.py
+++ multimolecule/models/rnamsm/modeling_rnamsm.py
@@ -87,7 +87,7 @@ class RnaMsmModel(RnaMsmPreTrainedModel):
         embedding_output = self.embeddings(input_ids, attention_mask=attention_mask)
         encoder_outputs = self.encoder(
             embedding_output,
-            key_padding_mask=attention_mask ^ 1,
+            key_padding_mask=(attention_mask ^ 1).bool(),
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
