801168747b75294baf5912678a18e4c8e221bf3a
Zhiyuan Chen <this@zyc.ai>
2024-04-23 13:58:14 +0800

    ensure pooler consistency
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- multimolecule/models/rnamsm/modeling_rnamsm.py
+++ multimolecule/models/rnamsm/modeling_rnamsm.py
@@ -1155,6 +1155,8 @@ class RnaMsmPooler(nn.Module):
         self.activation = nn.Tanh()
 
     def forward(self, hidden_states: Tensor) -> Tensor:
+        # We "pool" the model by simply taking the hidden state corresponding
+        # to the first token.
         first_token_tensor = hidden_states[:, 0]
         pooled_output = self.dense(first_token_tensor)
         pooled_output = self.activation(pooled_output)
