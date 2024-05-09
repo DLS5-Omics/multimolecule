c229a7dcd62de72e532f0b7946827a393bde3f16
Zhiyuan Chen <this@zyc.ai>
2024-05-09 18:33:41 +0800

    add copyright header
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- conftest.py
+++ conftest.py
@@ -1,2 +1,18 @@
+# MultiMolecule
+# Copyright (C) 2024-Present  MultiMolecule
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 collect_ignore = ["debug.py"]
 collect_ignore_glob = ["convert_checkpoint.py", "*ignore.py"]
