d44d84ae5619def266672299f2aa47850cb18d9a
Zhiyuan Chen <this@zyc.ai>
2026-02-02 01:28:51 +0800

    fix compatibility issues with newer pyarrow
    - add `large_string` check
    - add pa_type guard
    
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- multimolecule/data/dataset.py
+++ multimolecule/data/dataset.py
@@ -501,7 +501,13 @@ class Dataset(datasets.Dataset):
         self._sequence_cols = sequence_cols or []
         self._sequence_type = sequence_type  # type: ignore[assignment]
 
-        string_cols = [k for k, v in self.features.items() if k not in self.id_cols and v.pa_type == "string"]
+        string_cols = [
+            k
+            for k, v in self.features.items()
+            if k not in self.id_cols
+            and v.pa_type is not None  # noqa: W503
+            and (pa.types.is_string(v.pa_type) or pa.types.is_large_string(v.pa_type))  # noqa: W503
+        ]
         unique_chars = {
             k: {ch for s in flatten_column(self._data.column(k))[0] for ch in s.as_py()} for k in string_cols
         }
